dda31cd049d23c422e64508808f4d3a8049f5fd4
pappnu <pppskraga@gmail.com>
2026-04-05 12:51:51 +0300

    refactor: Code cleanup and auto formatting

--- src/templates/adventure.py
+++ src/templates/adventure.py
@@ -2,23 +2,19 @@
 * Adventure Templates
 """
 
-# Standard Library
-from functools import cached_property
 from collections.abc import Callable
+from functools import cached_property
 
-# Third Party Imports
 from photoshop.api._artlayer import ArtLayer
 from photoshop.api._layerSet import LayerSet
 
-# Local Imports
-from src.enums.layers import LAYERS
 import src.helpers as psd
-from src.schema.colors import GradientConfig
-from src.layouts import AdventureLayout, NormalLayout
-from src.schema.colors import ColorObject
-from src.templates._vector import VectorTemplate
-from src.templates._core import NormalTemplate
 import src.text_layers as text_classes
+from src.enums.layers import LAYERS
+from src.layouts import AdventureLayout, NormalLayout
+from src.schema.colors import ColorObject, GradientConfig
+from src.templates._core import NormalTemplate
+from src.templates._vector import VectorTemplate
 
 """
 * Modifier Classes
